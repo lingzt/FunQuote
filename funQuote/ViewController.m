7ec6cf4a82d4f744809e6cc9ba48d3fa5d7549a8
Ling <asklingtoby@gmail.com>
2016-05-24 21:46:40 -0400

    set up project

---
+++ funQuote/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  funQuote
+//
+//  Created by ling toby on 5/24/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+/*
+ Create an app that meets the following requirements:
+ Your app should display a humorous, random, or motivational quote on the screen
+ The quote should change to a new quote every 5 seconds (5-7 quotes total)
+ Each quote should have a random text color
+ Each quote should have a random background color
+ */
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
