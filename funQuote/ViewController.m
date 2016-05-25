d12b4a2f4571bc0a2216528a77c59b25cf0aae86
Ling <asklingtoby@gmail.com>
2016-05-25 06:50:06 -0400

    add ramdon function add timer

--- funQuote/ViewController.m
+++ funQuote/ViewController.m
@@ -24,7 +24,9 @@
 
 UIImage *chosenImage;
 NSString *chosenQuote;
+NSMutableArray *memesArr;
 BOOL spcialEffect;
+//NSTimer *repeadEveryFewSecond;
 
 @implementation ViewController
 /*
@@ -36,6 +38,8 @@ BOOL spcialEffect;
 //MARK: - Life Cycle Methods
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self timer];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -44,14 +48,40 @@ BOOL spcialEffect;
 }
 //MARK: - Interactivity Methods
 
+//1. preset Array
 -(void)presetArray{
     NSDictionary *meme1 = @{@"Image" : [UIImage imageNamed:@"1.png"],
                             @"Quote" : @[ @"Only character in Game of Thrones to died of old age"],
                             @"Special effects" : @NO,
                             };
-    NSMutableArray *memesArr = [NSMutableArray arrayWithObjects:meme1, nil];
+    NSDictionary *meme2 = @{@"Image" : [UIImage imageNamed:@"2.jpg"],
+                            @"Quote" : @[ @"The best kiler in game of Thrones?"],
+                            @"Special effects" : @NO,
+                            };
+    NSDictionary *meme3 = @{@"Image" : [UIImage imageNamed:@"3.png"],
+                            @"Quote" : @[ @"I heard the party died after I left"],
+                            @"Special effects" : @NO,
+                            };
+    memesArr = [NSMutableArray arrayWithObjects:meme1, meme2, meme3, nil];
 
 }
+
+
+-(void)showRandomMeme{
+    printf("timer is working \n");
+    NSUInteger r = arc4random_uniform([memesArr count]);
+    NSDictionary *chosenMeme = memesArr[r];
+    self.quoteLabel.text = chosenMeme[@"quote"];
+    
+    
+}
+
+-(void)timer{
+    NSTimer *repeadEveryFewSecond = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(showRandomMeme) userInfo:nil repeats:YES];
+    
+//    NSTimer *repeadEveryFewSecond = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector([self showRandomMeme)] userInfo:nil repeats:YES];
+}
+//
 
 
 
