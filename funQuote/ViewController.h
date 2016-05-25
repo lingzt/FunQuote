671d0016d946f2decb88ec1d3f1706e28c90fb6d
Ling <asklingtoby@gmail.com>
2016-05-24 22:29:04 -0400

    wire up conponments

--- funQuote/ViewController.h
+++ funQuote/ViewController.h
@@ -5,10 +5,18 @@
 //  Created by ling toby on 5/24/16.
 //  Copyright © 2016 Detroit Labs. All rights reserved.
 //
+/*
+ Your app should display a humorous, random, or motivational quote on the screen
+ The quote should change to a new quote every 5 seconds (5-7 quotes total)
+ Each quote should have a random text color
+ Each quote should have a random background color
+ */
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *quoteLabel;
 
 
 @end
--- funQuote/ViewController.m
+++ funQuote/ViewController.m
@@ -19,17 +19,25 @@
 @interface ViewController ()
 
 @end
+//MARK: - Properties
+
+
 
 @implementation ViewController
 
+
+//MARK: - Life Cycle Methods
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
+//MARK: - Interactivity Methods
+
+
 
 @end
